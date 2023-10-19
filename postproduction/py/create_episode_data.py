9a2b88812a241a6be36490fb316d524c2abdbc77
Gota Shirato / 白戸豪大 <gshirato@uni-bonn.de>
2023-10-19 15:47:08 +0200

    refactor

--- postproduction/py/create_episode_data.py
+++ postproduction/py/create_episode_data.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import os
-
 import sys
 from IOManager import IOManager
 from operate_filename import determine_episode_type_and_number
@@ -28,28 +27,55 @@ def create_topics_from_markers(markers):
     return response.choices[0].message.content.split("\n")
 
 
-def get_json_file(path):
-    if os.path.exists(path):
-        return IOManager.read_json(path)
-    return IOManager.read_json("json/template.json")
+def get_json_file(path, template_path="json/template.json"):
+    """
+    Retrieve JSON data from a file or fall back to a template if the file doesn't exist.
+    """
+    return (
+        IOManager.read_json(path)
+        if os.path.exists(path)
+        else IOManager.read_json(template_path)
+    )
+
+
+def manage_episode_data(episode_type, episode_number, markers):
+    """
+    Manages episode data: retrieves, updates and checks for edited episodes.
+
+    Parameters:
+    - episode_type: Type/genre of the episode.
+    - episode_number: Number of the episode.
+    - markers: List of markers used to generate topics.
+
+    Returns:
+    - Updated episode data.
+    """
+    episode_data = get_json_file(f"json/{episode_type}/{episode_number}.json")
+
+    if episode_data.get("Edited", False):
+        print(f"Episode {episode_number} is already edited.")
+        sys.exit()
+
+    episode_data["Number"] = episode_number
+    episode_data["Genre"] = episode_type
+    episode_data["Topics"].extend(create_topics_from_markers(markers))
+    episode_data["Edited"] = True
+
+    return episode_data
 
 
 def get_markers(path):
+    """
+    Extract markers from a CSV file.
+    """
     return IOManager.read_csv(path, sep="\t")["Name"].tolist()
 
 
 if __name__ == "__main__":
     episode_type, episode_number = determine_episode_type_and_number(sys.argv[1])
     markers = get_markers(f"markers/{episode_type}/{episode_number}.csv")
-    episode_data = get_json_file(f"json/{episode_type}/{episode_number}.json")
-    if episode_data.get("Edited", False):
-        print(f"Episode {episode_number} is already edited.")
-        sys.exit()
-    episode_data["Number"] = episode_number
-    episode_data["Genre"] = episode_type
-    topics = create_topics_from_markers(markers)
-    episode_data["Topics"].extend(topics)
-    episode_data["Edited"] = True
+
+    episode_data = manage_episode_data(episode_type, episode_number, markers)
 
     print(episode_data)
 
